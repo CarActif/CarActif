5b57d6b3e4de41249ca86ecfccdb91bfc10a95fe
CarActif <contactcharlauto@gmail.com>
2025-07-15 07:29:22 +0200

    Update Home.jsx

--- CarActifSite/src/pages/Home.jsx
+++ CarActifSite/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 import "../styles/Home.css";
+import { Link } from "react-router-dom";
+
 
 const supabase = createClient(
   "https://rhbnlcmsyqjbzykexttt.supabase.co",
