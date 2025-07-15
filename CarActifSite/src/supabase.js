26796c17c52ac94c802dda2347fcfb1d25052e44
CarActif <contactcharlauto@gmail.com>
2025-07-15 06:15:23 +0200

    Update supabase.js

--- CarActifSite/src/supabase.js
+++ CarActifSite/src/supabase.js
@@ -1,9 +1,8 @@
-// src/supabase.js
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://rhbnlcmsyqjbzykexttt.supabase.co' 
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJoYm5sY21zeXFqYnp5a2V4dHR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA0NDA5OTcsImV4cCI6MjA2NjAxNjk5N30.o24Li47McJpMVaa2sEjwR8oOa1mobsUqhdUUD2VwpT8' 
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
 
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-export default supabase
+export default supabase;
